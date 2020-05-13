ecc0ffcbd3a8781eba906f68775e4b28d1c54d7b
Usman <usman1kadir@gmail.com>
2020-05-13 02:40:44 +0000

    changes

--- BWT.cpp
+++ BWT.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include "BWT.h"
+#include <vector> 
+#include <algorithm>
 using namespace std;
 
 /**
@@ -7,4 +9,21 @@ using namespace std;
  */
 string bwt(const string & s) {
     /* YOUR CODE HERE */
+    vector<string> rotations;
+    string word = s;
+    string result;
+
+    for(unsigned int rotate = 0; rotate < s.length(); rotate++) {
+        word = word.substr(word.size()-1) + word.substr(0, word.size()-1);
+        rotations.push_back(word);
+    }
+
+    sort(rotations.begin(), rotations.end());
+
+    vector<string>::iterator it;
+    for(it = rotations.begin(); it < rotations.end(); it++) {
+        result = result + (*it)[s.length() - 1];
+    }
+
+    return result;
 }
--- SuffixArray.cpp
+++ SuffixArray.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <vector>
 #include "SuffixArray.h"
+#include <algorithm>
+
 using namespace std;
 
 /**
@@ -8,4 +10,21 @@ using namespace std;
  */
 vector<unsigned int> suffix_array(const string & s) {
     /* YOUR CODE HERE */
+    vector<string> suffixes;
+    vector<unsigned int> suffix_array;
+    for(unsigned int index = 0; index < s.length(); index++) {
+        string suffix = s.substr(index);
+        suffixes.push_back(suffix);
+    }
+    sort(suffixes.begin(), suffixes.end());
+
+    for(unsigned int si = 0; si < suffixes.size(); si++) {
+        for(unsigned int index = 0; index < s.length(); index++) {
+            if(s.substr(index) == suffixes[si]) {
+                suffix_array.push_back(index);
+            }
+        }
+    }
+
+    return suffix_array;
 }
